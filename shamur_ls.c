46f2e4ec4de845f83d6831c1c49c6608a04a6885
Ameenamurtala <139773220+Ameenamurtala@users.noreply.github.com>
2023-10-17 20:10:43 -0700

    Update shamur_ls.c

--- shamur_ls.c
+++ shamur_ls.c
@@ -7,19 +7,7 @@
  *
  * Return: negative
  */
-ssize_t get_node_index(mur_t *head, mur_t *node)
-{
-	size_t neg = 0;
 
-	while (head)
-	{
-		if (head == node)
-			return (neg);
-		head = head->next;
-		neg++;
-	}
-	return (-1);
-}
 
 
 /**
