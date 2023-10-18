e727644c7bac6efa7da15499dfb652b120992fcf
Ameenamurtala <139773220+Ameenamurtala@users.noreply.github.com>
2023-10-17 19:53:48 -0700

    Update murmem.c

--- murmem.c
+++ murmem.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * afrie - frees the memory of a pointer
- * @ptr: the pointer to be freed
+ * afrie - frees the memory of a pointa
+ * @ptr: the pointer 2 be freed
  *
- * Return: 1 if successful, 0 if not
+ * Return: 1 if successful 0 if not
  */
 int afrie(void **ptr)
 {
@@ -16,3 +16,4 @@ int afrie(void **ptr)
 	}
 	return (0);
 }
+
